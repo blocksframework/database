a124418b45b41bd4f378440c8b4128da5f603466
Stephen Stephenson <stephenson.inbox@gmail.com>
2024-04-01 09:36:17 +0200

    Feat: Add default mysql config

---
+++ config/default/mysql.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'mysql_hostname' => '127.0.0.1',
+    'mysql_port'     => '3306',
+    'mysql_username' => '',
+    'mysql_password' => '',
+    'mysql_database' => '',
+    'mysql_charset'  => 'utf8mb4_unicode_ci',
+];
